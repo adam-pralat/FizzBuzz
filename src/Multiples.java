900244f7a2c32113405370e0c6e282cd148a3ba2
mr-adamo <adamo.pralat@gmail.com>
2023-09-12 11:29:52 -0400

    Changed '|' to '||'

--- src/Multiples.java
+++ src/Multiples.java
@@ -2,7 +2,7 @@ public class Multiples {
     public static void main(String[] args) {
         int count = 0;
         for (int i = 1; i <= 1000; i++) {
-            if (i % 3 == 0 | i % 5 == 0) {
+            if (i % 3 == 0 || i % 5 == 0) {
                 count++;
             }
         }
